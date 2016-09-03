60a3c3d73a87d488a7fc36da5d6450705718fa13
liuxuanhai <liuxuanhai@163.com>
2016-09-03 08:26:58 +0800

    SDL 2.0
    Example For SDL 2.0 Cross Platform Programming

---
+++ Project/09_SnakeBite/Class/Common.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <vector>
+#include <time.h>
+#include <string>
+
+
+
+#define GAME_WIDTH  640
+#define GAME_HEIGHT 480
+
+void DrawSurface(SDL_Surface *dst, int x, int y, SDL_Surface *src);
+int BaseRand(int x, int y);
